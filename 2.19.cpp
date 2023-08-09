9b3f2980618b27567fc815d2192e4d951ab77fc1
TuanTran <anhtuans1702@gmail.com>
2023-08-09 21:42:02 +0700

    2.19.cpp

---
+++ 2.19.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2.19
+//
+//  Created by Thuy Ha on 09/08/2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int F;
+    cin >> F;
+    float C = (5*(F-32))/9;
+    cout << C;
+    return 0;
+}
