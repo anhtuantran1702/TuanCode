7b4ef87e8d0691da9cd5f702ab18df5f20a2359c
TuanTran <anhtuans1702@gmail.com>
2023-08-10 15:14:48 +0700

    ptbac2

---
+++ ptbac2.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  PTbac2
+//
+//  Created by Thuy Ha on 10/08/2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    float a;
+    float b;
+    float c;
+    Nhapdulieu:
+    cout << "Nhap a = ";
+    cin >> a;
+    if (a == 0) {cout << "a phai khac 0 \n";
+                    goto Nhapdulieu;}
+    else    {
+        cout << "Nhap b = ";
+        cin >> b;
+        cout << "Nhap c = ";
+        cin >> c;
+        float d = b*b - 4*a*c;
+        if (d<=0) {cout << "Phuong trinh vo nghiem\n";
+            goto Nhapdulieu;}
+        float x1 = (2*c)/(-b+sqrt(d));
+        float x2 = (2*c)/(-b-sqrt(d));
+        cout << "Phuong trinh co 2 nghiem phan biet \n" << x1 << " " << x2;
+    }
+    return 0;
+}
