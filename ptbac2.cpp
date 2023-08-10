c1d904ecd4f4eec40ffaacad5d193ae34882acd9
TuanTran <anhtuans1702@gmail.com>
2023-08-10 15:41:30 +0700

    ptbac2

--- ptbac2.cpp
+++ ptbac2.cpp
@@ -25,7 +25,8 @@ int main(int argc, const char * argv[]) {
         float d = b*b - 4*a*c;
         if (d<0) {cout << "Phuong trinh vo nghiem\n";
             goto Nhapdulieu;}
-        if (d==0) {cout << "Phuong trinh co nghiem kep \n" << (2*c)/(-b+sqrt(d));}
+        if (d==0) {cout << "Phuong trinh co nghiem kep \n" << (2*c)/(-b+sqrt(d));
+            return 0;}
         float x1 = (2*c)/(-b+sqrt(d));
         float x2 = (2*c)/(-b-sqrt(d));
         cout << "Phuong trinh co 2 nghiem phan biet \n" << x1 << " " << x2;
